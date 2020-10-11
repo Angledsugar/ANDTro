545c91748e8e01d029ffb4aa8fcc05885283b747
ccy <angledsugar@gmail.com>
2020-10-11 22:01:37 +0900

    Revert "ANDTRO_v2"
    This reverts commit f64890e5b1206a166c714c831cb7443dbabaf0e2.

--- andtro/andtro.ino
+++ andtro/andtro.ino
@@ -1,26 +1,15 @@
 #include <DynamixelWorkbench.h>
 #include <IMU.h>
-#include <Servo.h>
 
-Servo bldc;
-int pos = 0;
-
-#define BAUDRATE             115200
+#define BAUDRATE              57600
 #define BAUDRATE_TO_DXL     1000000
 #define LEFT_FRONT_ID             1
 #define RIGHT_FRONT_ID            2
 #define LEFT_BACK_ID              3
 #define RIGHT_BACK_ID             4 
-#define esc                       6
 
 DynamixelWorkbench dxl_wb;
 cIMU IMU;
-int sp = 40;
-int gs = 1;
-int bs = 1;
-int rs = 1;
-int ls = 1;
-char c = 's';
 
 uint8_t   err_code;
 uint8_t   led_tog = 0;
@@ -31,16 +20,10 @@ void setup() {
     Serial.begin(BAUDRATE);
     //while(!Serial); // Open a Serial Monitor
     IMU.begin();
-    
-    bldc.attach(esc);
-    bldc.write(pos);
-    c = 's';
-    
-    /*pinMode( 22, OUTPUT );
+    pinMode( 22, OUTPUT );
     pinMode( 23, OUTPUT );
     pinMode( 24, OUTPUT );
-    pinMode( 25, OUTPUT );*/
-    
+    pinMode( 25, OUTPUT );
     dxl_wb.begin("", BAUDRATE_TO_DXL);
     dxl_wb.ping(LEFT_FRONT_ID);
     dxl_wb.ping(RIGHT_FRONT_ID);
@@ -73,185 +56,89 @@ void loop() {
   if( (millis()-tTime[1]) >= 50 )
   {
     tTime[1] = millis();
-    /*
-    Serial.print(imu_time);
+
+    /*Serial.print(imu_time);
     Serial.print(" ");
     Serial.print(IMU.rpy[0]);
     Serial.print(" ");
     Serial.print(IMU.rpy[1]);
     Serial.print(" ");
-    Serial.println(IMU.rpy[2]);
-    */
+    Serial.println(IMU.rpy[2]);*/
   }
- 
-  
-  /*
-  if     ( 30 <= IMU.rpy[0] && IMU.rpy[0] <  50) bldc.write(110);
-  else if( 50 <= IMU.rpy[0] && IMU.rpy[0] <  70) bldc.write(130);
-  else if(( 70 <= IMU.rpy[0] && IMU.rpy[0] < 180 )||( -180 < IMU.rpy[0] && IMU.rpy[0] < -90)) bldc.write(140);
-  
-  else if(100 <= IMU.rpy[0] && IMU.rpy[0] < 150) bldc.write(130);
-  else if(150 <= IMU.rpy[0] && IMU.rpy[0] < 215) bldc.write(140);
- 
-  else bldc.write(90);*/
-  
   
   if(Serial.available()){
-    c = Serial.read();
+    char c = Serial.read();
     
     if( c == 'w'){ //Go
-      //digitalWrite(22, LOW);
+      digitalWrite(22, LOW);
       //delay(500);
-      go();
-      gs++;
-      bs = 1;
-      rs = 1;
-      ls = 1;
-      
+      //go();
     }
 
     else if( c == 'x'){ //Back
-      //digitalWrite(23, LOW);
+      digitalWrite(23, LOW);
       //delay(500);
-      back();
-      bs++;
-      gs = 1;
-      rs = 1;
-      ls = 1;
+      //back();
     }
 
     else if( c == 'a'){ //Left
-      //digitalWrite(24, LOW);
-      //Serial.print("A\n");
+      digitalWrite(24, LOW);
+      Serial.print("A\n");
       //delay(500);
-      left();
-      ls++;
-      gs = 1;
-      bs = 1;
-      rs = 1;
+      //left();
     }
 
     else if( c == 'd'){ //Right
-      //digitalWrite(25, LOW);
+      digitalWrite(25, LOW);
       //delay(500);
-      right();
-      rs++;
-      gs = 1;
-      ls = 1;
-      bs = 1;
+      //right();
     }
     
     else if( c == 's'){ //Stops
       //c = '0';
-      /*Serial.print("Stop\n");
+      Serial.print("Stop\n");
       digitalWrite(22, HIGH);
       digitalWrite(23, HIGH);
       digitalWrite(24, HIGH);
       digitalWrite(25, HIGH);
-      //delay(500);*/
-      stops();
-      pos = 90;
-      gs = 1;
-      bs = 1;
-      ls = 1;
-      rs = 1;
+      //delay(500);
+      //stops();
     }
-    else if( c == '0'){ //Start:edfpulse_0
-      //c = '0';
-      /*Serial.print("Stop\n");
-      digitalWrite(22, HIGH);
-      digitalWrite(23, HIGH);
-      digitalWrite(24, HIGH);
-      digitalWrite(25, HIGH);
-      //delay(500);*/
-      bldc.write(0);
-    }
-    else if( c == '1'){ //edfpulse_100
-      //c = '0';
-      /*Serial.print("Stop\n");
-      digitalWrite(22, HIGH);
-      digitalWrite(23, HIGH);
-      digitalWrite(24, HIGH);
-      digitalWrite(25, HIGH);
-      //delay(500);*/
-      bldc.write(100);
-    }
-    else if( c == '2'){ //edfpulse_110
-      //c = '0';
-      /*Serial.print("Stop\n");
-      digitalWrite(22, HIGH);
-      digitalWrite(23, HIGH);
-      digitalWrite(24, HIGH);
-      digitalWrite(25, HIGH);
-      //delay(500);*/
-      bldc.write(110);
-    }
-    else if( c == '3'){ //edfpulse_120
-      //c = '0';
-      /*Serial.print("Stop\n");
-      digitalWrite(22, HIGH);
-      digitalWrite(23, HIGH);
-      digitalWrite(24, HIGH);
-      digitalWrite(25, HIGH);
-      //delay(500);*/
-      bldc.write(120);
-    }
-    else if( c == '4'){ //edfpulse_130
-      //c = '0';
-      /*Serial.print("Stop\n");
-      digitalWrite(22, HIGH);
-      digitalWrite(23, HIGH);
-      digitalWrite(24, HIGH);
-      digitalWrite(25, HIGH);
-      //delay(500);*/
-      bldc.write(130);
-    }
-    else if( c == '5'){ //edfpulse_140
-      //c = '0';
-      /*Serial.print("Stop\n");
-      digitalWrite(22, HIGH);
-      digitalWrite(23, HIGH);
-      digitalWrite(24, HIGH);
-      digitalWrite(25, HIGH);
-      //delay(500);*/
-      bldc.write(140);
-     }
-    c = 0;
   }
 }
 
 void go() {
-    dxl_wb.goalVelocity(LEFT_FRONT_ID, sp*gs);
-    dxl_wb.goalVelocity(RIGHT_FRONT_ID, -sp*gs);
-    dxl_wb.goalVelocity(LEFT_BACK_ID, sp*gs);
-    dxl_wb.goalVelocity(RIGHT_BACK_ID, -sp*gs);
-    //delay(2000);
+    dxl_wb.goalVelocity(LEFT_FRONT_ID, 150);
+    dxl_wb.goalVelocity(RIGHT_FRONT_ID, -150);
+    dxl_wb.goalVelocity(LEFT_BACK_ID, 150);
+    dxl_wb.goalVelocity(RIGHT_BACK_ID, -150);
+    delay(2000);
 }
 
 void back(){
-    dxl_wb.goalVelocity(LEFT_FRONT_ID, -sp*bs);
-    dxl_wb.goalVelocity(RIGHT_FRONT_ID, sp*bs);
-    dxl_wb.goalVelocity(LEFT_BACK_ID, -sp*bs);
-    dxl_wb.goalVelocity(RIGHT_BACK_ID, sp*bs);
-    //delay(2000);
+    dxl_wb.goalVelocity(LEFT_FRONT_ID, -150);
+    dxl_wb.goalVelocity(RIGHT_FRONT_ID, 150);
+    dxl_wb.goalVelocity(LEFT_BACK_ID, -150);
+    dxl_wb.goalVelocity(RIGHT_BACK_ID, 150);
+    delay(2000);
 }
 
 void right(){
     // put your main code here, to run repeatedly:
-    dxl_wb.goalVelocity(LEFT_FRONT_ID, sp*rs);
-    dxl_wb.goalVelocity(RIGHT_FRONT_ID, sp*rs);
-    dxl_wb.goalVelocity(LEFT_BACK_ID, sp*rs);
-    dxl_wb.goalVelocity(RIGHT_BACK_ID, sp*rs);
-    //delay(2000);
+    dxl_wb.goalVelocity(LEFT_FRONT_ID, 150);
+    dxl_wb.goalVelocity(RIGHT_FRONT_ID, 150);
+    dxl_wb.goalVelocity(LEFT_BACK_ID, -150);
+    dxl_wb.goalVelocity(RIGHT_BACK_ID, -150);
+    delay(2000);
 }
 
 void left(){
     // put your main code here, to run repeatedly:
-    dxl_wb.goalVelocity(LEFT_FRONT_ID, -sp*ls);
-    dxl_wb.goalVelocity(RIGHT_FRONT_ID, -sp*ls);
-    dxl_wb.goalVelocity(LEFT_BACK_ID, -sp*ls);
-    dxl_wb.goalVelocity(RIGHT_BACK_ID, -sp*ls);
-    //delay(2000);
+    dxl_wb.goalVelocity(LEFT_FRONT_ID, -150);
+    dxl_wb.goalVelocity(RIGHT_FRONT_ID, -150);
+    dxl_wb.goalVelocity(LEFT_BACK_ID, 150);
+    dxl_wb.goalVelocity(RIGHT_BACK_ID, 150);
+    delay(2000);
 }
 
 void stops(){
@@ -260,7 +147,7 @@ void stops(){
     dxl_wb.goalVelocity(RIGHT_FRONT_ID, 0);
     dxl_wb.goalVelocity(LEFT_BACK_ID, 0);
     dxl_wb.goalVelocity(RIGHT_BACK_ID, 0);
-    //delay(2000);
+    delay(2000);
 }
 
 //bool goalVelocity(uint8_t id, float velocity, const char **log = NULL);
